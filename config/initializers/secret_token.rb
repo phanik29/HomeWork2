1e9e77dc474c40e88e8fd42148787a5d33d47326
Phani Kumar K <phanik29@gmail.com>
2012-09-25 04:13:38 -0600

    Create Blog project
    Created Block Project with Aptana

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HomeWork2::Application.config.secret_token = 'd69fa878df8d572fb62600706daee7bd3b018c7a098883f3ccb56e0afb88b79539e15574b7beffd078d71f00f3c20ba80d8f6e92d5f623a854ed7d5165613627'
